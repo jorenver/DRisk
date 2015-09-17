16388622cde647ab03517cf6f9b128db8fc3fa3e
obayona <obayona@espol.edu.ec>
2015-09-17 13:52:38 -0500

    Join in a match

--- app/controller/WaitRoom.js
+++ app/controller/WaitRoom.js
@@ -1,18 +1,34 @@
 var html_dir = './app/views/';
 
 exports.waitroom = function(request, response){
+	
+	var idGame = request.query.idmatch;
+	var name = request.query.idcreator;
+	console.log("waitron idGmae", idGame);
+	console.log("unirse***", idGame , name);
+
 	response.render('waitroom', {idGame: request.query.idmatch});
+	var matches = request.session.matches;
+	matches[idGame].listPlayer.push(name); 
+	console.log(matches[idGame].listPlayer);
+
 };
 
 
 exports.players = function(request, response){
 	
 	var idGame = request.query.idGame;
+	console.log("idGame***",idGame);
 	
-	var list = [ "Mafia Chumi", "Fajada1", "Fajada2", "Fajada3", "Fajada4", "Fajada5"];
+	var matches = request.session.matches;
+
+	console.log("players", matches[idGame].listPlayer);
+	var list = matches[idGame].listPlayer;
+	
 	var dataGame = {
 		mode: "Mundial Domination"
 	}
+
 	var j = {players:list, dataGame: dataGame};
 	response.json(j);	
 };
