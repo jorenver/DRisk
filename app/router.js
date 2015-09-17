fb40afffb206d0be4fa1cae0b79afb16c8925d9c
obayona <obayona@espol.edu.ec>
2015-09-16 22:52:36 -0500

    Views for choose match

--- app/router.js
+++ app/router.js
@@ -1,9 +1,16 @@
 var gameListController = require('./controller/GamesList');
+var waitRoomController = require('./controller/WaitRoom');
+var startGameController = require('./controller/StartGame');
 
 var express=require('express');
 var router=express.Router();
 
 router.get('/games',gameListController.list);
 router.post('/matches',gameListController.matches);
+
+router.get('/waitroom', waitRoomController.waitroom);
+router.get('/players', waitRoomController.players);
+router.get('/startGame', startGameController.startGame);
+
 
 module.exports = router;
