06166a75b4b746eed76b3073adc0e1f5a2cf2346
rodrigo <rodfcast@espol.edu.ec>
2015-09-16 23:41:20 -0500

    Correcion en Matches

--- app/controller/Matches.js
+++ app/controller/Matches.js
@@ -1,3 +1,6 @@
+
+var matches = {};
+
 exports.index = function(request,response){
 	response.render('index');
 }
@@ -10,6 +13,12 @@ exports.setParameters = function(request,response){
 	if(request.session){
 		request.session.match = { nickname : request.query.nickname ,
 			maxPlayer : request.query.maxPlayer, mode : request.query.mode
+		}
+		matches[ request.query.nickname ] = { maxPlayer: request.query.maxPlayer,
+			mode : request.query.mode	
+		};
+		for(var key in matches){
+			console.log(matches[key]);
 		}
 		response.send(request.session.match);
 	}else{
